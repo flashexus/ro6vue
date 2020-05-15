5a61ccb7645cbee6791d3adb5d84bba4a45fc923
flashexus <32187820+flashexus@users.noreply.github.com>
2020-05-15 16:12:40 +0900

    起動時のスクリプトファイルを修正

--- docker/Rails6/scripts/launch.sh
+++ docker/Rails6/scripts/launch.sh
@@ -3,5 +3,8 @@
 bundle exec rake db:migrate
 cp -rf /tmp/public/* /Ro6MysNgx/public/
 mkdir -p tmp/sockets
+mkdir -p tmp/db
+mkdir -p tmp/public
+mkdir -p tmp/pids
 bundle exec puma -C config/puma.rb
 
