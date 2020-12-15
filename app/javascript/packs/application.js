0d21476244fd3aa44dec5096304db46f670a9163
iwata_k <iwata_k@vitallead.co.jp>
2020-12-15 11:54:14 +0900

    iOS用テスト

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -47,45 +47,6 @@ if ( deviceType == 'Android' && browerType != 'Chrome' ) {
 }
 
 ///////////////////////////////////////////////////////////////////////////////
-//メニュー表示用
-//$(window).on('load', function(){
-  var menu = $('.vl-menu');
-  // var modalBtn = $('.vl-modalBtn');
-  // var modalBtnClose = $('.vl-modalBtnClose');
-  // var modalBtnCloseFix = $('.vl-modalBtnCloseFix');
-  var modalBg = $('.vl-modalBg');
-  var modalMain = $('.vl-modalMain');
-  //メニュー開く
-  menu.on('click','.vl-modalBtn',function (e) {
-    $(this).next(modalBg).fadeIn();
-    $(this).next(modalBg).next(modalMain).removeClass("_slideDown");
-    $(this).next(modalBg).next(modalMain).addClass("_slideUp");
-  });
-  //メニュー閉じ
-  menu.on('click','.vl-modalBtnClose',function (e) {
-    modalBg.fadeOut();
-    modalMain.removeClass("_slideUp");
-    modalMain.addClass("_slideDown");
-  });
-  //モーダル展開
-  menu.on('click','.vl-modalMain',function (e) {
-    e.stopPropagation();
-  });
-  //モーダル背景
-  menu.on('click','.vl-modalBg',function (e) {
-    $(this).fadeOut();
-    $(this).next(modalMain).removeClass("_slideUp");
-    $(this).next(modalMain).addClass("_slideDown");
-  });
-
-
-//   modalBtnCloseFix.on('click', function (e) {
-//     modalBg.fadeOut();
-//     modalMain.removeClass("_slideUp");
-//     modalMain.addClass("_slideDown");
-//   });
-// });
-///////////////////////////////////////////////////////////////////////////////
 // ユーザーエージェントからブラウザタイプを判定
 function judgeBrowserType(UserAgent){
   if(UserAgent.indexOf('msie') != -1 ||
