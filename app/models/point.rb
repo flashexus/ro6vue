2d26e8621631ce2ec9630d23a74ed49321e5335a
iwata_k@vital <iwata_k@vitallead.co.jp>
2021-01-05 16:02:54 +0900

    iphone用修正

--- app/models/point.rb
+++ app/models/point.rb
@@ -1,6 +1,6 @@
 class Point < ApplicationRecord
     has_many :stamps, dependent: :destroy
-    INIT_AREA = "石央エリア"
+    INIT_AREA = "石西エリア"
     #area group Const
     AREA__GROUP_TYPE = %w(石西エリア 石央エリア 石東エリア)
     #area group Const
